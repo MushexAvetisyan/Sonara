436485a151deb0913fccbe1e5225f5acf880e861
MushexAvetisyan <avetisyan.mushex@inbox.ru>
2026-09-13 01:37:58 +0400

    fix deployment paths for GitHub Pages and Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/sonara/',
+
+  base: process.env.GITHUB_ACTIONS
+      ? '/sonara/'
+      : '/',
 })
