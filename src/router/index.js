a08101c454f408eb0196aa4d7297e86fa7256684
MushexAvetisyan <avetisyan.mushex@inbox.ru>
2026-09-13 00:57:12 +0400

    fix: configure GitHub Pages and Sass

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import Home from '../views/Home.vue'
 import History from '../views/History.vue'
@@ -6,7 +6,7 @@ import Favorites from '../views/Favorites.vue'
 import Playlists from '../views/Playlists.vue'
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
 
     routes: [
         {
